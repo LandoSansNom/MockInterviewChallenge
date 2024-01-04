a15d98096eedd18042b67855095a17208a1c0d67
consultant <Philippe.cherlan-miche@techconsulting.tech>
2024-01-04 14:37:32 -0500

    Add the dispatcher to the repository

--- app/src/main/java/com/cherlan/mockinterviewchallenge/data/repository/CountryRepositoryImpl.kt
+++ app/src/main/java/com/cherlan/mockinterviewchallenge/data/repository/CountryRepositoryImpl.kt
@@ -2,9 +2,11 @@ package com.cherlan.mockinterviewchallenge.data.repository
 
 import com.cherlan.mockinterviewchallenge.data.model.Country
 import com.cherlan.mockinterviewchallenge.utils.Result
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 
 class CountryRepositoryImpl: CountryRepository {
@@ -13,6 +15,6 @@ class CountryRepositoryImpl: CountryRepository {
         return flow {
             val countries = listOf(Country("United States of America","NA","USA", "Washington D.C"),Country("Uruguay","SA","UY", "Montevideo"))
             emit(Result.Success(countries))
-        }
+        }.flowOn(Dispatchers.IO)
     }
 }
